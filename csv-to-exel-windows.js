27c61f52793a9254ff7d516f2119b811895173bb
emanuele-ricci <82324327+emanuele-ricci@users.noreply.github.com>
2021-04-11 17:12:26 +0200

    Update and rename csv-to-exel.js to csv-to-exel-windows.js

--- csv-to-exel-windows.js
+++ csv-to-exel-windows.js
@@ -1,12 +1,9 @@
 
 const { exec } = require("child_process");
 const path = require('path');
-
 const  xl = require('excel4node');
-
 const csv = require('csv-parser');
 const fs = require('fs');
-
 
 const wb = new xl.Workbook();
 const ws = wb.addWorksheet('Sheet 1');
@@ -17,57 +14,35 @@ const regex2 = /^\d+$/g;
 let key_array = [];
 let value_array = [];
 
-
-
 function csv_filter(value) {
-
   const csv_only = value.match(regex);
   return csv_only ;
-
 };
 
 function convert(file_path, file_name){
 
   return new Promise((resolve,reject)=>{
-
     fs.createReadStream(file_path)
     .pipe(csv())
-      
     .on('data', (row) => {
-    
       for (const [key] of Object.entries(row)) {
-      
         key_array.push(key);
-      
       }
-    
       value_array.push(Object.values(row));
-    
     })
 
     .on('end', () => {
-
       const header = [ ...new Set(key_array)];  
-
       let k = 1; 
-
       for(let i=0;i<header.length;i++){
-
         ws.cell(1,k).string(header[i])
-
         let c = 2;
-
         for(let a=0;a<value_array.length;a++){
-
           (value_array[a][i].match(regex2)) ? ws.cell(c,k).number(Number(value_array[a][i])) : ws.cell(c,k).string(value_array[a][i]);
           c++;
-
         }
-
         k++;
-
       }
-
 
       wb.write(`Parsed/${file_name[0]}.xlsx`);
       key_array = [];
@@ -75,38 +50,25 @@ function convert(file_path, file_name){
       resolve(`Parsed/${file_name[0]}.xlsx DONE`);
 
     });
-
   });
-
 };
 
-
-
 const wd = new Promise((resolve,reject)=>{
-
   const command = "echo %cd%";
-
   const pwd = function (command,callback){
     exec(command, (error,stdout, stderr)=>{
       callback(stdout)
     });
   };
-
   pwd(command,(callback)=>{
-
     const arr = callback.split("\r");
     arr.pop();
     resolve(arr);
-
   });
-
 });
 
-
 async function Do(){
-
   await wd.then((dir)=>{
-    
     exec("dir /b", async (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
@@ -132,32 +94,15 @@ async function Do(){
       const files = stdout.split("\n");
       const filtered = files.filter(csv_filter);
 
-    
-      for(let d=0; d<filtered.length;d++){
-    
+      for(let d=0; d<filtered.length;d++){    
         const file_name = filtered[d].split('.');
-        
         const file_path = path.join(dir[0],filtered[d]).split('\r');
         console.log(file_path[0]);
-
         await convert(file_path[0], file_name).then(res=>console.log(res)).catch(err=>console.log(err));
-
-    
       };
-
       setTimeout(()=>{},3000);
-      
     });
-
   });
-
-  
 };
 
 Do();
-
-
-
-
-
-
