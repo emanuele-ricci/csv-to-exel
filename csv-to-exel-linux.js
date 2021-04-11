bef2558b8c5bbc8362dd56fb7632ff3024ffa324
emanuele-ricci <82324327+emanuele-ricci@users.noreply.github.com>
2021-04-11 17:06:16 +0200

    Update and rename csv-to-exel.js to csv-to-exel-linux.js

--- csv-to-exel-linux.js
+++ csv-to-exel-linux.js
@@ -1,8 +1,6 @@
 
 const { exec } = require("child_process");
-
 const  xl = require('excel4node');
-
 const csv = require('csv-parser');
 const fs = require('fs');
 
@@ -16,72 +14,42 @@ let key_array = [];
 let value_array = [];
 
 
-
 function csv_filter(value) {
-
   const csv_only = value.match(regex);
   return csv_only ;
-
 }
 
 function convert(filtered, file_name){
-
+  
   return new Promise((resolve,reject)=>{
-
     fs.createReadStream(filtered)
-
     .pipe(csv())
-      
     .on('data', (row) => {
-
-      for (const [key] of Object.entries(row)) {
-        
+      for (const [key] of Object.entries(row)) {  
         key_array.push(key);
-      
       }
-    
       value_array.push(Object.values(row));
-
     })
-
     .on('end', () => {
-
       const header = [ ...new Set(key_array)]; 
-      
       let k = 1; 
-
       for(let i=0;i<header.length;i++){
-
         ws.cell(1,k).string(header[i])
-
         let c = 2;
-
         for(let a=0;a<value_array.length;a++){
-
-
-
           (value_array[a][i].match(regex2)) ? ws.cell(c,k).number(Number(value_array[a][i])) : ws.cell(c,k).string(value_array[a][i]);
           c++;
-
         }
-
         k++;
-
       }
-
 
     wb.write(`./Parsed/${file_name[0]}.xlsx`);
     key_array = [];
     value_array = [];
     resolve(`./Parsed/${file_name[0]}.xlsx DONE`);
-
     });
-
   });
-
-
 }
-
 
 exec("ls", async (error, stdout, stderr) => {
   if (error) {
@@ -92,7 +60,6 @@ exec("ls", async (error, stdout, stderr) => {
     console.log(`stderr: ${stderr}`);
     return;
   }
-  
   exec("mkdir Parsed"),(error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
@@ -108,11 +75,8 @@ exec("ls", async (error, stdout, stderr) => {
   const filtered = files.filter(csv_filter);
 
   for(let d=0; d<filtered.length;d++){
-
     const file_name = filtered[d].split('.');
-
     await convert(filtered[d], file_name).then(res=>console.log(res)).catch(err=>console.log(err));
-
   }
   
 });
